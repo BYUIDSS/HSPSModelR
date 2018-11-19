195141ac96ba787743ac09c0d5f6fcc85ed7c806
Chad Schaeffer <sch12059@byui.edu>
2018-11-18 20:08:40 -0700

    make_table and make_list functions

---
+++ R/make_list_help_file.R
@@ -0,0 +1,20 @@
+#' Make a list of models
+#'
+#' Compiles the contents of a folder into a list. Intended for machine learning models, and for use with the BYUImachine::make_table function
+#'
+#' @param x source or file path
+#' @import stingr
+#' @import rlist
+#' @return This function returns a \code{list} of contents at specified location
+#'
+#' @author "Chad Schaeffer <sch12059@@byui.edu>, Brad Borget <bor13001@@byui.edu>"
+
+make_list <- function(x) {
+  file.names <- dir(x, pattern =".rds")
+  list <- list()
+  for(i in 1:length(file.names)){
+    file <- readRDS(str_c(x, file.names[i], collapse = ""))
+    list <- list.append(list, file)
+  }
+  return(list)
+}
---
+++ R/make_table_help_file.R
@@ -0,0 +1,68 @@
+#' Make a machine learning diagnostics table
+#'
+#' Takes a list of trained machine learning models and returns diagnostics as a data frame, as to compare the effectiveness of algorithms. Measures include Accuracy, Prevalence, Detection Rate, F1, Cohen's Kappa, McNemar P-Value, Negative and Positive Predictive value, Precision, Recall, Sensitivity, and Specificity
+#'
+#' @param x A list of models
+#'
+#' @import caret
+#' @import dplyr
+#' @import tibble
+#'
+#' @return This function returns a \code{data.frame} including columns:
+#' \itemize{
+#'  \item Type
+#'  \item Accuracy
+#'  \item Accuracy Lower
+#'  \item Accuracy Null
+#'  \item Accuracy P value
+#'  \item Accuracy Upper
+#'  \item Balanced Accuracy
+#'  \item Detection Prevalence
+#'  \item Detection Rate
+#'  \item F1
+#'  \item Kappa
+#'  \item McNemar P Value
+#'  \item Negagive Prediction Value
+#'  \item Positive Predictive Value
+#'  \item Precision
+#'  \item Prevalence
+#'  \item Recall
+#'  \item Sensitivity
+#'  \item Specificty
+#'  \item Method
+#' }
+#'
+#' @author "Chad Schaeffer <sch12059@@byui.edu>"
+#'
+
+make_table <- function(x) {
+  make_row <- function(i) {
+    p <- predict(i, test)
+    t <- confusionMatrix(table(p, test$Truth))
+
+    as.data.frame(t[4]) %>%
+      rownames_to_column(var = "measure") %>%
+      rename_at("byClass", ~ "name") %>%
+      bind_rows((as.data.frame(t[3])) %>%
+                  rownames_to_column(var = "measure") %>%
+                  rename_at("overall", ~ "name")) %>%
+      mutate(type = "name") %>%
+      spread(measure, name) %>%
+      mutate(method = i[1]) %>%
+      select(-type)
+  }
+
+  row_list <- lapply(list, make_row)
+
+  table <- data.frame(matrix(unlist(row_list),
+                             nrow = length(row_list),
+                             ncol = max(vapply(row_list, length, 0)),
+                             byrow = TRUE))
+
+  names <- c("Accuracy", "AccuracyLower", "AccuracyNull", "AccuracyPValue", "AccuracyUpper",
+             "Balanced Accuracy", "Detection Prevalence", "Detection Rate", "F1",
+             "Kappa", "McnemarPValue", "Neg Pred Value", "Pos Pred Value",
+             "Precision", "Prevalence", "Recall", "Sensitivity", "Specificity", "Method")
+  colnames(table) <- names
+  return(table)
+}
