844fe6f65dc61472fae842f83de780fe51de86c6
Chad Schaeffer <sch12059@byui.edu>
2018-11-30 21:04:37 -0700

    finished common_predictions forever

--- R/common_predictions_help_file.R
+++ R/common_predictions_help_file.R
@@ -1,10 +1,11 @@
 #' Find at risk customers
 #'
-#' Makes predictions using a list of models and returns a vector where each value represents a customer which has been predicted to drop by a pre-specified percentage of models.
+#' Makes predictions using a list of models and returns a vector where each value represents the ratio of models which agree on a specified classification for that observation.
 #'
 #' @param x A list of models
+#' @param factor the specific target for which wou want to pull the strongest common predictions.
 #' @param test_data a df, the portion of data you are using to test your predictions.
-#' @param threshold a value between 0 and 1. What ratio of columns do you need to agree on the "Dropped" value
+#' @param threshold a value between 0 and 1. What ratio of columns do you need to agree your target factor?
 #'
 #' @importFrom stringr str_replace_all
 #' @importFrom stats predict
@@ -20,18 +21,45 @@
 #' @author "Chad Schaeffer <sch12059@@byui.edu>"
 
 
-get_likely_drops <- function(x, test_data, threshold) {
-  dat <- test_data
+get_common_predictions <- function(x, test_data, factor, threshold) {
+  if (!is.data.frame(test_data) | !is_tibble(test_data)) {
+    stop("test_data needs to be a data.frame or tibble")
+  } else {
+    test <- test_data
+  }
 
   make_predictions <- function(i) {
-    predict(i, dat, type = "raw") %>%
-      str_replace_all(c("Healthy" = "0", "Dropped" = "1")) %>%
-      as.integer()
+    p <- predict(i, test, type = "raw")
+
+    if (!is.factor(p)) {
+      stop("all models must be able to produce predictions using stats::predict()")
+    }
+
+    if (str_detect(str_c(p, collapse = "|"), factor) == FALSE) {
+      stop("factor must be in the target column you used to train your models")
+    } else {
+      p %>%
+        as.factor() %>%
+        as.tibble() %>%
+        transmute(new = case_when(
+          value %in% factor ~ 1,
+          TRUE ~ 0
+        ))
+    }
   }
 
   predictions_array <- map_dfc(x, make_predictions)
-  likely_drops <- predictions_array %>%
-    transmute(percent_dropped = (rowSums(predictions_array) / ncol(predictions_array))) %>%
-    filter(.data$percent_dropped >= threshold)
-  return(likely_drops)
+
+  if (threshold > 1 | threshold < 0){
+    stop("threshold must be an integer between 0 and 1")
+  }
+  else {
+    common_predictions <- predictions_array %>%
+      transmute(percent_common = (rowSums(predictions_array) / ncol(predictions_array))) %>%
+      filter(percent_common >= threshold)
+  }
+
+  return(common_predictions)
 }
+
+
